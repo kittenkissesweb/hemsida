d601a79f3d68be0abe3ca62223ecb2a22c51caa3
Git Import <git@localhost>
2025-01-12 15:46:36 +0100

    Socializing is hard

---
+++ content/socializing_is_hard.md
@@ -0,0 +1,5 @@
+Title: Socializing is hard
+Date: 2025-01-12 10:20
+Category: Comic
+
+![Socializing is hard](comics/socializing.png)
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+AUTHOR = 'kittenkisses'
+SITENAME = 'Kittenkisses'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Stockholm'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (
+    ("tiktok", "https://www.tiktok.com/@emosupportskeleton"),
+    ("instagram", "https://www.instagram.com/emosupportskeleton/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'pelican-purple'
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+DISPLAY_SUMMARY = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+STATIC_PATHS = [
+    'comics',
+]
